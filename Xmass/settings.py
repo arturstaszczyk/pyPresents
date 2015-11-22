fa38b8a58e3ff2dc4776e0d88553e65e9f9b0f33
Artur Staszczyk <astaszczyk@ganymede.eu>
2015-11-22 16:47:00 +0100

    Changes for 2015 begins ;)

--- Xmass/settings.py
+++ Xmass/settings.py
@@ -81,17 +81,17 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-DATABASES['default'] =  dj_database_url.config()
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-ALLOWED_HOSTS = ['*']
-
+DEBUG = True
 STATIC_ROOT = 'staticfiles'
 
-DEBUG = False
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+# Sth for deployment
+#DATABASES['default'] =  dj_database_url.config()
+#
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+#
+# ALLOWED_HOSTS = ['*']
+#
+# try:
+#     from .local_settings import *
+# except ImportError:
+#     pass
